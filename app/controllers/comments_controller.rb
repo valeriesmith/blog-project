06720b339174361ce2072545817ec1e3f053d48f
Valerie Smith <valeriesmithdbc@gmail.com>
2015-02-09 14:15:44 -0800

    adds comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+
+	# index show new edit create update destroy
+
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+	    @post = Post.find(params[:post_id])
+	    @comment = @post.comments.find(params[:id])
+	    @comment.destroy
+	 
+	    redirect_to post_path(@post)
+  	end
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,10 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def create
 	    @post = Post.new(post_params)
 	 
@@ -21,10 +25,26 @@ class PostsController < ApplicationController
     		redirect_to @post
   		else
     		render 'new'
-    		p @post.errors
-    		p @post.errors.full_messages
   		end
   	end
+
+  	def update
+  		@post = Post.find(params[:id])
+
+  		if @post.update(params[:post].permit(:title, :body))
+      		redirect_to @post
+    	else
+      		render 'edit'
+    	end
+  	end
+
+  	def destroy
+  		@post = Post.find(params[:id])
+  		@post.destroy
+
+  		redirect_to posts_path
+  	end
+
 
     private
  
